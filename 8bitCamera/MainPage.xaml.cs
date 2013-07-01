1fb7a0b962e459707121f4df42f3162bb1878a4b
Peter Morlion <peter.morlion@live.be>
2013-07-01 17:54:48 +0200

    Initial working version

---
+++ 8bitCamera/MainPage.xaml.cs
@@ -0,0 +1,197 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Devices;
+using Microsoft.Xna.Framework.Media;
+using System.Windows.Navigation;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Windows.Media.Imaging;
+
+namespace EightBitCamera
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private int savedCounter = 0;
+        PhotoCamera _photoCamera;
+        MediaLibrary mediaLibrary = new MediaLibrary();
+        private Pixelator _pixelator = new Pixelator(8);
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (PhotoCamera.IsCameraTypeSupported(CameraType.Primary))
+            {
+                _photoCamera = new PhotoCamera(CameraType.Primary);
+                _photoCamera.Initialized += OnCameraInitialized;
+                _photoCamera.CaptureCompleted += OnCameraCaptureCompleted;
+                _photoCamera.CaptureImageAvailable += OnCameraCaptureImageAvailable;
+                _photoCamera.CaptureThumbnailAvailable += OnCameraCaptureThumbnailAvailable;
+                
+                viewfinderBrush.SetSource(_photoCamera);
+            }
+            else
+            {
+                // TODO: handle possibility of no camera
+            }
+        }
+
+        private void OnCameraInitialized(object sender, CameraOperationCompletedEventArgs e)
+        {
+            // TODO: fix Timer
+            int timer = 0;
+            while (timer <= 100)
+            {
+                UpdateViewFinder(null);
+                timer++;
+            }
+            //var timer = new Timer(UpdateViewFinder, null, 0, 1000);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            if (_photoCamera != null)
+            {
+                _photoCamera.Dispose();
+
+                _photoCamera.CaptureCompleted -= OnCameraCaptureCompleted;
+                _photoCamera.CaptureImageAvailable -= OnCameraCaptureImageAvailable;
+                _photoCamera.CaptureThumbnailAvailable -= OnCameraCaptureThumbnailAvailable;
+            }
+        }
+
+        private void ShutterButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_photoCamera == null)
+                return;
+
+            _photoCamera.CaptureImage();
+        }
+
+        private void LoadThumbnails()
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(delegate()
+            {
+                thumbnailsPanel.Children.Clear();
+            });
+
+            using (var isolatedStorageFile = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                var fileNames = isolatedStorageFile.GetFileNames("*_th.jpg");
+                foreach (var fileName in fileNames)
+                {
+                    byte[] data;
+                    using (var isolatedStorageFileStream = isolatedStorageFile.OpenFile(fileName, FileMode.Open, FileAccess.Read))
+                    {
+                        data = new byte[isolatedStorageFileStream.Length];
+                        isolatedStorageFileStream.Read(data, 0, data.Length);
+                        isolatedStorageFileStream.Close();
+                    }
+                    Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                    {
+                        var memoryStream = new MemoryStream(data);
+                        var bitmap = new BitmapImage();
+                        bitmap.SetSource(memoryStream);
+                        var image = new Image();
+                        image.Height = 24;
+                        image.Width = 24;
+                        image.Source = bitmap;
+                        thumbnailsPanel.Children.Add(image);
+                    });
+                }
+            }
+        }
+
+        private void UpdateViewFinder(object state)
+        {
+            var width = (int) _photoCamera.PreviewResolution.Width;
+            var height = (int) _photoCamera.PreviewResolution.Height;
+            int max = width * height;
+            int[] buffer = new int[max];
+            _photoCamera.GetPreviewBufferArgb32(buffer);
+
+            buffer = _pixelator.Pixelate(buffer, width, height);
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+                                                          {
+                                                              var wb = new WriteableBitmap(width, height);
+                                                              bitPreview.Source = wb;
+                                                              buffer.CopyTo(wb.Pixels, 0);
+                                                              wb.Invalidate();
+                                                          });
+        }
+
+        private void OnCameraCaptureImageAvailable(object sender, ContentReadyEventArgs e)
+        {
+            var fileName = "8bitImage" + savedCounter + ".jpg";
+            try
+            {
+                mediaLibrary.SavePictureToCameraRoll(fileName, e.ImageStream);
+                e.ImageStream.Seek(0, SeekOrigin.Begin);
+                using (var isolatedStorageFile = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (var targetStream = isolatedStorageFile.OpenFile(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        var readBuffer = new byte[4096];
+                        var bytesRead = -1;
+
+                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                        {
+                            targetStream.Write(readBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                e.ImageStream.Close();
+            }
+        }
+
+        private void OnCameraCaptureCompleted(object sender, CameraOperationCompletedEventArgs e)
+        {
+            savedCounter++;
+        }
+
+        private void OnCameraCaptureThumbnailAvailable(object sender, ContentReadyEventArgs e)
+        {
+            var fileName = "8bitImage" + savedCounter + "_th.jpg";
+            try
+            {
+                using (var isolatedStorageFile = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (var targetStream = isolatedStorageFile.OpenFile(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        byte[] readBuffer = new byte[4096];
+                        int bytesRead = -1;
+
+                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                        {
+                            targetStream.Write(readBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+                LoadThumbnails();
+            }
+            finally
+            {
+                e.ImageStream.Close();
+            }
+        }
+    }
+}
---
+++ 8bitCamera/Pixelator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace EightBitCamera
+{
+    public class Pixelator
+    {
+        private readonly int _pixelateSize;
+
+        public Pixelator(int pixelateSize)
+        {
+            _pixelateSize = pixelateSize;
+        }
+
+        public int[] Pixelate(int[] buffer, int width, int height)
+        {
+            // 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8
+            // 9 , 10, 11, 12, 13, 14, 15, 16
+            // 17, 18, 19, 20, 21, 22, 23, 24
+            // 25, 26, 27, 28, 29, 30, 31, 32
+            // 33, 34, 35, 36, 37 ,38, 39, 40
+            // 41, 42, 43, 44, 45, 46, 47, 48
+            // 49, 50, 51, 52, 53, 54, 55, 56
+            // 57, 58, 59, 60, 61, 62, 63, 64
+
+            // 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8, 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37 ,38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64
+
+            // 19, 19, 19, 19, 23, 23, 23, 23
+            // 19, 19, 19, 19, 23, 23, 23, 23
+            // 19, 19, 19, 19, 23, 23, 23, 23
+            // 19, 19, 19, 19, 23, 23, 23, 23
+            // 51, 51, 51, 51, 55, 55, 55, 55
+            // 51, 51, 51, 51, 55, 55, 55, 55
+            // 51, 51, 51, 51, 55, 55, 55, 55
+            // 51, 51, 51, 51, 55, 55, 55, 55
+
+            // 19, 19, 19, 19, 23, 23, 23, 23, 19, 19, 19, 19, 23, 23, 23, 23, 19, 19, 19, 19, 23, 23, 23, 23, 19, 19, 19, 19, 23, 23, 23, 23, 51, 51, 51, 51, 55, 55, 55, 55, 51, 51, 51, 51, 55, 55, 55, 55, 51, 51, 51, 51, 55, 55, 55, 55, 51, 51, 51, 51, 55, 55, 55, 55
+
+            // 12 -> 22
+            // 13 -> 22 
+            // 22 -> 22
+            // 23 -> 22
+            // 28 -> 22
+            for (int i = 0; i < width * height; i++)
+            {
+                var y = (int)((double)i / width);
+                var offsetX = 0;
+                var referencePixel = 0;
+
+                var x = 0;
+
+                var isNewPixelatedLine = y % _pixelateSize == 0;
+
+                if (isNewPixelatedLine)
+                {
+                    x = i + (_pixelateSize / 2) - (i % _pixelateSize);
+                    var offsetY = y % _pixelateSize;
+                    offsetX = offsetY + (_pixelateSize / 2) - (offsetY % _pixelateSize);
+                    referencePixel = x + (offsetX * width);
+                }
+                else
+                {
+                    referencePixel = i - ((y % _pixelateSize) * width);
+                }
+                
+                // TODO: do something with last line(s)
+                if (referencePixel < buffer.Length)
+                {
+                    buffer[i] = buffer[referencePixel];
+                }
+            }
+
+            return buffer;
+        }
+    }
+}
